c8580812a781df359dc286c7d27170f17fe95998
MC Weaponry ADM <mcweaponry.adm@mcweaponry.com>
2025-10-26 17:49:44 -0600

    Affect CSS on team photos.

--- src/App.svelte
+++ src/App.svelte
@@ -226,8 +226,8 @@
 
   .team-card img {
     width: 100%;
-    height: 250px;
-    object-fit: cover;
+    height: 300px;
+    object-fit: contain;
     border-radius: 8px 8px 0 0;
     margin-bottom: 1.5rem;
   }
